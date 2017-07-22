06189f077e9f914a936d9208baf4817c9f398488
Charles Rea <charles.rea@softwire.com>
2017-07-22 15:55:21 +0100

    Add hello world API

---
+++ src/main/kotlin/net/charlesrea/wedding/Greeting.kt
@@ -0,0 +1,3 @@
+package net.charlesrea.wedding
+
+data class Greeting(val id: Long, val content: String)
---
+++ src/main/kotlin/net/charlesrea/wedding/GreetingController.kt
@@ -0,0 +1,17 @@
+package net.charlesrea.wedding
+
+import java.util.concurrent.atomic.AtomicLong
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetingController {
+
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
+
+}
