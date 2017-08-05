31f65ac6dc472fc163a56640f8e8e8ebc2dc44c8
Charles Rea <charles.rea@softwire.com>
2017-08-05 21:04:30 +0100

    Correct annotation processing

--- server/src/main/kotlin/net/charlesrea/wedding/security/AuthenticationSettings.kt
+++ server/src/main/kotlin/net/charlesrea/wedding/security/AuthenticationSettings.kt
@@ -1,11 +1,13 @@
 package net.charlesrea.wedding.security
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Component
 
 
 @Component
 @ConfigurationProperties(prefix = "authentication")
+@EnableConfigurationProperties
 class AuthenticationSettings {
     lateinit var key: String
 }
