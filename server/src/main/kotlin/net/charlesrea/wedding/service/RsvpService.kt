a6aedc7d1f0c6349e4d9ab18e027ccfce12b36c2
Charles Rea <charles.rea@softwire.com>
2017-08-27 19:55:12 +0100

    Add simple RSVP emailing

--- server/src/main/kotlin/net/charlesrea/wedding/service/RsvpService.kt
+++ server/src/main/kotlin/net/charlesrea/wedding/service/RsvpService.kt
@@ -2,9 +2,30 @@ package net.charlesrea.wedding.service
 
 import net.charlesrea.wedding.model.Rsvp
 import net.charlesrea.wedding.repository.RsvpRepository
+import org.springframework.mail.MailSender
 import org.springframework.stereotype.Service
+import org.springframework.mail.MailException
+import org.springframework.mail.SimpleMailMessage
 
 @Service
-class RsvpService(val rsvpRepository: RsvpRepository) {
-    fun createRsvp(rsvp: Rsvp) = rsvpRepository.save(rsvp)
+class RsvpService(val rsvpRepository: RsvpRepository, val mailSender: MailSender) {
+    fun createRsvp(rsvp: Rsvp): Rsvp {
+        rsvpRepository.save(rsvp)
+        sendEmail(rsvp);
+        return rsvp;
+    }
+
+    private fun sendEmail(rsvp: Rsvp) {
+        val message = SimpleMailMessage()
+        message.to = arrayOf("wedding@harrietandcharles.com")
+        message.from = "wedding@harrietandcharles.com"
+        message.subject = "RSVP received from ${rsvp.name}"
+        message.text = rsvp.toString()
+        try {
+            mailSender.send(message)
+        } catch (ex: MailException) {
+            // simply log it and go on...
+            System.err.println(ex.message);
+        }
+    }
 }
